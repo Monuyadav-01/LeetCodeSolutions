72dd12af4db7d4edb9a0ef593a00e567b4645b07
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-09-09 23:35:29 +0530

    Time: 1 ms (69.94%) | Memory: 40.5 MB (74.41%) - LeetSync

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+          min=Math.min(min,nums[i]);
+          max=Math.max(max, nums[i]);
+        }
+        int cnt=0;
+        for(int i=0;i<nums.length;i++){
+          if(nums[i] !=min && nums[i] !=max) cnt++;
+        }
+        return cnt;
+    }
+}
