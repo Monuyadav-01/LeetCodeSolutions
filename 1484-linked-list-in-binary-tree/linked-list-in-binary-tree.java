69bc9b71be3d84a4170607a48380e32ee371554c
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-09-12 22:27:52 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (83.57%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isSubPath(ListNode head, TreeNode root) {
+//         if(root==null) return false;
+//         if(head==null) return true;
+//         return( helper(head,root) || isSubPath(head, root.left) || isSubPath(head, root.right));
+//     }
+//     public boolean helper(ListNode head, TreeNode root){
+//        if(root==null) return false;
+//         if(head==null) return true;
+//         return (head.val == root.val) && (helper(head.next ,root.left) || helper(head.next, root.right));
+//     }
+// }
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        return (isStartPath(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right));
+    }
+
+    private boolean isStartPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        return (head.val == root.val) && (isStartPath(head.next, root.left) || isStartPath(head.next, root.right));
+    }
+}
