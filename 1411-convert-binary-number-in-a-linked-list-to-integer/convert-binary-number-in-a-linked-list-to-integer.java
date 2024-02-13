cbefecd1ae46d097870293b749967536cac75c81
Monu <105378383+Monuyadav-01@users.noreply.github.com>
2024-02-13 20:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (60.28%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
@@ -12,7 +12,7 @@ class Solution {
     public int getDecimalValue(ListNode head) {
         int num = head.val;
         while(head.next != null){
-             num = 2*num+head.next.val;
+            num = 2*num+head.next.val;
             head = head.next;
         }
         return num;
