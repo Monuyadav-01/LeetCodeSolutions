0d6c214472ef0cb63edef7719da1896ed2aa6e77
Monu <105378383+Monuyadav-01@users.noreply.github.com>
2024-02-18 20:37:47 +0530

    Time: 31 ms (24.58%) | Memory: 45.4 MB (62.06%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isAlphabetic(ch)) continue;
+            if(ch == '('){
+                st.push(i);
+            }
+            else{
+                if(!st.isEmpty() && s.charAt(st.peek()) == '('){
+                    st.pop();
+                }
+                else{
+                    st.push(i);
+                }
+            }
+        }
+
+        HashSet<Integer> set = new HashSet<>(st);
+        for(int i =0;i<s.length();i++){
+            if(!set.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+}
