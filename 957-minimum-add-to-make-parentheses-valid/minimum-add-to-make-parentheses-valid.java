3df07599febcd32119eb677b6c8b4cb383b36ab1
Monu <105378383+Monuyadav-01@users.noreply.github.com>
2024-02-27 15:57:53 +0530

    Time: 1 ms (59.10%) | Memory: 41.7 MB (10.00%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int ans =0;
+        int bal =0;
+
+        for(int i =0;i<s.length();i++){
+            bal+=s.charAt(i) == '('?1 :-1;
+
+            if(bal == -1){
+                ans++;
+                bal++;
+            }
+        }
+        return ans+bal;
+    }
+}
