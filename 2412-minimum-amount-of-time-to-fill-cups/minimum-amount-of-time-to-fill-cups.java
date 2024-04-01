7636316a058e19d9e4641709400e180e6c741ac3
Monu <105378383+Monuyadav-01@users.noreply.github.com>
2024-04-01 09:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (92.67%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.java
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.java
@@ -1,25 +1,12 @@
 class Solution {
     public int fillCups(int[] amount) {
-        int ans =0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int i :amount){
-            pq.add(i);
+      int sum = 0;
+        int max = 0;
+        for (int i : amount) {
+            max = Math.max(max, i);
+            sum += i;
         }
-        while(pq.peek() != 0){
-            int t1= pq.poll();
-            int t2 = pq.poll();
 
-            if(t2!=0){
-                pq.add(t1-1);
-                pq.add(t2-1);
-                ans++;
-            }
-            else{
-                return ans + t1;
-            }
-        }
-        return ans;
-
+        return Math.max(max, (sum + 1) / 2);
     }
 }
